810133f42af9f2b317aa339f7150ae7ad40f0760
Anthony A Davis <35675472+anthonyadavisii@users.noreply.github.com>
2018-12-20 21:49:41 -0600

    Update blacklist_check.py

--- blacklist_check.py
+++ blacklist_check.py
@@ -7,14 +7,14 @@ bot = Bot(description='Blacklist Checker', command_prefix='?')
 @bot.command()
 async def blacklist_check(user):
     """Queries blacklist status of user using @themarkymark's Blacklist API."""
-    contents = urllib.request.urlopen("http://blacklist.usesteem.com/user/"+user).read()
-    bl = (str(contents).split('"blacklisted":[')[1]).split(']')[0]
+    contents = urllib.request.urlopen("http://blacklist.usesteem.com/user/"+user).read() #Get request to pull JSON object
+    bl = (str(contents).split('"blacklisted":[')[1]).split(']')[0] #extracts blacklists from byte object
     if bl == '':
         await bot.say('@'+user+' is not on any blacklist tracked via the API')
         return
     else:
         blacklists = []
-        bl = bl.split(',')
+        bl = bl.split(',') #splits in case of multiple blacklist entries with comma delimiter
         for b in bl:
             blacklists.append(b.replace('"',''))
         await bot.say('@'+user+' is on the following blacklists: ')
